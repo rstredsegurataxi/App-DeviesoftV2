5fcb35196c4a14051b4054edbc3ad1642c162e6c
Yeixon Bastidas <ybastidas@logyca.com>
2022-01-24 21:34:23 -0500

    se agrega card de quienes somos

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import logo from './logo.jpg'
+// import hands from '../../imgs/favicon.ico'
 
 const About = () => {
   return <div>
-    <img  src={logo} style={{width:'50%'}} />
+   
       <section class="info page-section mt-5" id="about">
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center align-items-center">
                     <div class="col-lg-8 text-center ">
-                        <h2 class="text-black mb-5">Nosotros</h2>
-                        <Link to="/contacto" class="cta">
-                            <span>Comenzar</span>
-                            <svg width="13px" height="10px" viewBox="0 0 13 10">
-                                <path d="M1,5 L11,5"></path>
-                                <polyline points="8 1 12 5 8 9"></polyline>
-                            </svg>
-                        </Link>
+                        <h2 class="text-black mb-5">¿Quiénes somos?</h2>
+
+                        <div class="card" >
+                            {/* <img src={hands} class="card-img-top cardwith" alt="..."/> */}
+                            <div class="card-body">
+                               <p><strong>Deviesoft</strong>, Somos una compañía creada por amigos con un fin en común,
+                               lograr solucionar las problemáticas del día a día respaldados con la tecnología, 
+                               actualmente estamos enfocados en las problemáticas de las medianas y pequeñas empresas, para 
+                               que logren llegar al mercado y competir mano a mano con empresas de alto calibre y experiencia.
+                              </p>
+                            </div>
+                        </div>
                     </div>                    
                 </div>             
             </div>
